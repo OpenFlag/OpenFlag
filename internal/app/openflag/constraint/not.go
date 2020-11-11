afd284276540ff6c8d4c34ee2e3ddc0fab5b35a3
Rashad Ansari <rashad.ansari1996@gmail.com>
2020-11-11 18:02:54 +0330

    feat: Add not constraint

--- internal/app/openflag/constraint/not.go
+++ internal/app/openflag/constraint/not.go
@@ -1,9 +1,15 @@
 package constraint
 
-import "github.com/OpenFlag/OpenFlag/internal/app/openflag/model"
+import (
+	"github.com/OpenFlag/OpenFlag/internal/app/openflag/model"
+	validation "github.com/go-ozzo/ozzo-validation"
+)
 
 // NotConstraint represents Openflag not constraint.
-type NotConstraint struct{}
+type NotConstraint struct {
+	constraint    Constraint
+	RawConstraint model.Constraint `json:"constraint"`
+}
 
 // Name is an implementation for the Constraint interface.
 func (n NotConstraint) Name() string {
@@ -12,15 +18,27 @@ func (n NotConstraint) Name() string {
 
 // Validate is an implementation for the Constraint interface.
 func (n NotConstraint) Validate() error {
-	return nil
+	return validation.ValidateStruct(&n,
+		validation.Field(
+			&n.constraint,
+			validation.Required,
+		),
+	)
 }
 
 // Initialize is an implementation for the Constraint interface.
 func (n *NotConstraint) Initialize() error {
+	c, err := New(n.RawConstraint)
+	if err != nil {
+		return err
+	}
+
+	n.constraint = c
+
 	return nil
 }
 
 // Evaluate is an implementation for the Constraint interface.
 func (n NotConstraint) Evaluate(e model.Entity) bool {
-	return false
+	return !n.constraint.Evaluate(e)
 }
---
+++ internal/app/openflag/constraint/not_test.go
@@ -0,0 +1,118 @@
+package constraint_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/OpenFlag/OpenFlag/internal/app/openflag/constraint"
+	"github.com/OpenFlag/OpenFlag/internal/app/openflag/model"
+	"github.com/stretchr/testify/suite"
+)
+
+type MotConstraintSuite struct {
+	ConstraintSuite
+}
+
+func (suite *MotConstraintSuite) TestMotConstraint() {
+	cases := []ConstraintTestCase{
+		{
+			Name: "successfully create constraint and evaluate 1",
+			Constraint: model.Constraint{
+				Name: constraint.NotConstraintName,
+				Parameters: json.RawMessage(
+					fmt.Sprintf(
+						`
+						{
+							"constraint": {
+								"name": "%s",
+								"parameters": {
+									"value": 10
+								}
+							}
+						}
+					`,
+						constraint.LessThanConstraintName,
+					),
+				),
+			},
+			ErrExpected: false,
+			Entity: model.Entity{
+				ID: 15,
+			},
+			EvaluateExpected: true,
+		},
+		{
+			Name: "successfully create constraint and evaluate 2",
+			Constraint: model.Constraint{
+				Name: constraint.NotConstraintName,
+				Parameters: json.RawMessage(
+					fmt.Sprintf(
+						`
+						{
+							"constraint": {
+								"name": "%s",
+								"parameters": {
+									"value": 10
+								}
+							}
+						}
+					`,
+						constraint.LessThanConstraintName,
+					),
+				),
+			},
+			ErrExpected: false,
+			Entity: model.Entity{
+				ID: 5,
+			},
+			EvaluateExpected: false,
+		},
+		{
+			Name: "failed to create constraint (creation of inside constraint)",
+			Constraint: model.Constraint{
+				Name: constraint.NotConstraintName,
+				Parameters: json.RawMessage(
+					fmt.Sprintf(
+						`
+						{
+							"constraint": {
+								"name": "%s",
+								"parameters": {
+									"value": "10""
+								}
+							}
+						}
+					`,
+						constraint.LessThanConstraintName,
+					),
+				),
+			},
+			ErrExpected: true,
+			Entity: model.Entity{
+				ID: 15,
+			},
+			EvaluateExpected: false,
+		},
+		{
+			Name: "failed to create constraint (invalid parameters)",
+			Constraint: model.Constraint{
+				Name: constraint.NotConstraintName,
+				Parameters: json.RawMessage(
+					`{}`,
+				),
+			},
+			ErrExpected: true,
+			Entity: model.Entity{
+				ID: 5,
+			},
+			EvaluateExpected: false,
+		},
+	}
+
+	suite.RunCases(cases)
+}
+
+func TestMotConstraintSuite(t *testing.T) {
+	suite.Run(t, new(MotConstraintSuite))
+}
